c859941f9424aa4a929afb049ddd7127ce7d259d
Jessie Hsieh <jessiehsieh86@gmail.com>
2022-08-24 15:34:24 +0800

    Add user decide range

--- guess.py
+++ guess.py
@@ -1,12 +1,19 @@
-#tell the user the answer is highwe/lower if the user answer wrong
-#r is the random number
-#g is the number user guessed
+# tell the user the answer is highwe/lower if the user answer wrong
+# add count
+# add user decide range
+
 import random
-r = random.randint(1, 100)
+start = input('Please decide the starting number for the range:')
+start = int(start)
+end = input('Please decide the ending number for the range:')
+end = int(end)
+
+r = random.randint(start, end) #r is the random number
 count = 0
+
 while True:
 	count += 1 # count =  count + 1
-	g = input('Please guess a number:')
+	g = input('Please guess a number:') #g is the number user guessed
 	g = int(g)
 	if g == r:
 		print('Bingo!')
